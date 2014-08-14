e5064ba293c0c0aabaeddbe60a48899fb6ae5c26
Ben Pickles <spideryoung@gmail.com>
2014-08-14 17:09:33 +0100

    Ask the ladder about this.

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -2,34 +2,4 @@ module PlayersHelper
   def players_as_options
     ['---'] + Player.alphabetical.map{|e| [e.name, e.id] }
   end
-
-  def sparkline_max
-    [min_elo_value, max_elo_value].map(&:abs).max
-  end
-
-  def sparkline_min
-    -sparkline_max
-  end
-
-  def sparkline_central_value
-    @sparkline_central_value ||= EloRating.instance.initial_rating
-  end
-
-  private
-
-  def previous_scores
-    @previous_scores ||= Result.all.flat_map { |result|
-      result.previous_state.fetch('elo_ratings', {}).values.compact
-    }
-  end
-
-  def min_elo_value
-    min = previous_scores.min
-    min ? min - sparkline_central_value : sparkline_central_value
-  end
-
-  def max_elo_value
-    max = previous_scores.max
-    max ? max - sparkline_central_value : sparkline_central_value
-  end
 end
--- app/models/ladder.rb
+++ app/models/ladder.rb
@@ -41,4 +41,33 @@ class Ladder
   def rankings
     Player.in_elo_order.map(&Ranking)
   end
+
+  def score_history_max
+    [min_elo_value, max_elo_value].map(&:abs).max
+  end
+
+  def score_history_min
+    -score_history_max
+  end
+
+  private
+    def min_elo_value
+      min = previous_scores.min
+      min ? min - sparkline_central_value : sparkline_central_value
+    end
+
+    def max_elo_value
+      max = previous_scores.max
+      max ? max - sparkline_central_value : sparkline_central_value
+    end
+
+    def previous_scores
+      @previous_scores ||= Result.all.flat_map { |result|
+        result.previous_state.fetch('elo_ratings', {}).values.compact
+      }
+    end
+
+    def sparkline_central_value
+      @sparkline_central_value ||= EloRating.instance.initial_rating
+    end
 end
