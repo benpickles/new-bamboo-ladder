f1ed301c769e2d59f50e7be49a805b483aacd9d8
Ben Pickles <spideryoung@gmail.com>
2014-08-14 15:58:48 +0100

    Introduce the Ranking object that contains information about a Player’s place in the ladder.

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -3,12 +3,6 @@ module PlayersHelper
     ['---'] + Player.alphabetical.map{|e| [e.name, e.id] }
   end
 
-  def sparkline_data_for(player)
-    recorded_values = player.results.in_order.last(50).map(&:previous_state).map{|state| state['elo_ratings'].fetch(player.id){ nil } }
-    values = recorded_values + [player.elo_rating]
-    values.map{|v| v ? v - sparkline_central_value : 0 }.join(',')
-  end
-
   def sparkline_max
     [min_elo_value, max_elo_value].map(&:abs).max
   end
--- app/models/ladder.rb
+++ app/models/ladder.rb
@@ -1,9 +1,44 @@
 class Ladder
+  class Ranking
+    def self.to_proc
+      ->(player) { new(player) }
+    end
+
+    def initialize(player)
+      @player = player
+    end
+
+    def player_name
+      player.name
+    end
+
+    def position
+      player.position
+    end
+
+    def score
+      player.elo_rating
+    end
+
+    def score_history
+      recorded_values = player.results.in_order.last(50).map(&:previous_state).map{|state| state['elo_ratings'].fetch(player.id){ nil } }
+      values = recorded_values + [player.elo_rating]
+      values.map{|v| v ? v - score_central_value : 0 }
+    end
+
+    private
+      attr_reader :player
+
+      def score_central_value
+        @score_central_value ||= EloRating.instance.initial_rating
+      end
+  end
+
   def self.current
     new
   end
 
-  def players
-    Player.in_elo_order
+  def rankings
+    Player.in_elo_order.map(&Ranking)
   end
 end
